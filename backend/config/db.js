22833d4091ca33afc2fd58d1cccebdbd1d4faf7e
JashanjotSingh812 <122783310+JashanjotSingh812@users.noreply.github.com>
2025-05-17 14:53:23 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://jashan0812:Yoyohoneysingh0812@cluster0.wixif.mongodb.net/food-del').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect('mongodb+srv://jashanjot635:Yoyohoneysingh2512@cluster0.cq0hvd4.mongodb.net/food-del').then(()=>console.log("DataBase Connected"));
 }
